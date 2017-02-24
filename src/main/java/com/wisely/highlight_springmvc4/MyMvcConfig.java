b3c082ee85e53776554874706350514880b4e750
chenjinming <chenjinming1@xunlei.com>
2017-02-24 18:07:40 +0800

    first commit

---
+++ src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
@@ -0,0 +1,24 @@
+package com.wisely.highlight_springmvc4;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Created by chenjinming on 2017/2/24.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.wisely.highlight_springmvc4")
+public class MyMvcConfig {
+
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/classes/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+}
